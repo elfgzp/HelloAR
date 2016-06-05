b119339522e2e5cfe0646828e6e6f04ad0650f8d
Gzp_ <741424975@qq.com>
2016-06-06 00:39:34 +0800

    Change BarCodeInfo.cs

--- Assets/HelloAR/Scripts/BarCodeInfo.cs
+++ Assets/HelloAR/Scripts/BarCodeInfo.cs
@@ -6,10 +6,18 @@ using System.Collections;
     {
         public string targetImageUrl { get; set; }
         public string prefabUrl { get; set; }
+		public string commentApi { get; set; }
+		public string getCommentsApi { get; set; }
+		public string likeApi { get; set; }
+		public string getLikesApi { get; set; }
 
 	public BarCodeInfo() {
         	targetImageUrl = "";
         	prefabUrl = "";
+			commentApi = "";
+			getCommentsApi = "";
+			likeApi = "";
+			getLikesApi = "";
         }
     }
 
