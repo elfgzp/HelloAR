3871a7fa02c2124349f08c887ebd57617e75fa81
Gzp_ <741424975@qq.com>
2016-08-11 07:04:37 +0800

    changes

--- Assets/HelloAR/Scripts/GyroHandler.cs
+++ Assets/HelloAR/Scripts/GyroHandler.cs
@@ -22,9 +22,9 @@ public class GyroHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (!gyroEnabled)
